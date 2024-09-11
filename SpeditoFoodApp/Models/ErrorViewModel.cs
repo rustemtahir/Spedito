144a4433d0c5820ce5aa994e9098ea72afc94657
rustemtahir <rustammemmedeliyev@gamil.com>
2024-09-11 12:49:34 +0400

    init

---
+++ SpeditoFoodApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpeditoFoodApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
